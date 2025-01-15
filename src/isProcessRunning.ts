1c6b1a963e4e632104bbdaa797fd00fd2f961693
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2025-01-15 16:21:04 -0500

    Improve `isProcessRuning` function

--- src/isProcessRunning.ts
+++ src/isProcessRunning.ts
@@ -3,13 +3,18 @@ import * as cp from "child_process";
 export default function isProcessRunning(processName: string): Promise<boolean> {
   return new Promise((resolve) => {
     const platform = process.platform;
-    const cmd = platform === "win32" ? `tasklist` : `ps -ax`;
+
+    const cmd = platform === "win32" ? `tasklist` : `ps -ax | grep -i ${processName} | grep -v grep`;
 
     cp.exec(cmd, (err, stdout) => {
       if (err) {
         resolve(false);
       } else {
-        resolve(stdout.toLowerCase().indexOf(processName.toLowerCase()) > -1);
+        const isRunning =
+          platform === "win32"
+            ? stdout.toLowerCase().includes(processName.toLowerCase())
+            : stdout.split("\n").some((line) => line.toLowerCase().includes(processName.toLowerCase()));
+        resolve(isRunning);
       }
     });
   });
