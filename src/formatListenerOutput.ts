e7193a2b719f8cd3eabf8fcc73a68bb9ad405939
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2025-02-16 17:37:17 -0600

    minor edit

--- src/formatListenerOutput.ts
+++ src/formatListenerOutput.ts
@@ -6,22 +6,47 @@ const YELLOW = "\x1b[33m";
 const CYAN = "\x1b[36m";
 const RESET = "\x1b[0m";
 const ITALICS = "\x1b[3m";
+const RED = "\x1b[31m";
+const GREEN = "\x1b[92m";
+const BLUE = "\x1b[38;2;65;105;225m"; // RGB for royal blue
+
+enum MessageType {
+  MESSAGE,
+  ERROR,
+  WARNING,
+  BUG,
+}
 
 export default function formatListenerOutput(strings: string[]): string {
-  if (strings.length === 0) return "";
+  if (strings.length < 3) return "";
+  // get message type
+  const typeIndex: number = Number(strings[0]);
+  const type = [MessageType.MESSAGE, MessageType.ERROR, MessageType.WARNING, MessageType.BUG][Number.isNaN(typeIndex) ? 0 : typeIndex];
   // Color the header (first string) in orange.
-  const header = strings[0] === "" ? "" : `${ITALICS}${ORANGE}${strings[0]}${RESET} • `;
+  const header = strings[1] === "" ? "" : `${ITALICS}${ORANGE}${strings[1]}${RESET} • `;
   // Join the remaining strings.
-  let content = prettyPrint(strings.slice(1).join(" "));
+  let content = strings.slice(2).join(" ");
+  switch (type) {
+    case MessageType.ERROR:
+      content = `${RED}${content}${RESET}`;
+      break;
+    case MessageType.WARNING:
+      content = `${GREEN}${content}${RESET}`;
+      break;
+    case MessageType.BUG:
+      content = `${BLUE}${content}${RESET}`;
+      break;
+    default:
+      // Replace square brackets and numbers with their colored versions.
+      content = prettyPrint(content).replace(/(\[|\]|\b(-?\d+(\.\d+)?)\b)/g, (match) => {
+        if (match === "[" || match === "]") {
+          return `${YELLOW}${match}${RESET}`;
+        } else {
+          return `${CYAN}${match}${RESET}`;
+        }
+      });
+      break;
+  }
 
-  // Replace square brackets and numbers with their colored versions.
-  content = content.replace(/(\[|\]|\b(-?\d+(\.\d+)?)\b)/g, (match) => {
-    if (match === "[" || match === "]") {
-      return `${YELLOW}${match}${RESET}`;
-    } else {
-      return `${CYAN}${match}${RESET}`;
-    }
-  });
-
-  return `${header}${content}`;
+  return `${RESET}${header}${content}`;
 }
